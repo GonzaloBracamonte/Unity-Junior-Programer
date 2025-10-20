5fe6cadda93af295bc8fb3657536c650be55dd9a
Gonzalo Bracamonte <94326143+GonzaloBracamonte@users.noreply.github.com>
2025-10-20 17:43:45 -0300

    Prototype 4 - Unity program

---
+++ Project 4/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    private float spawnRange = 9;
+    public int enemyCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnEnemyWave(3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyCount = FindObjectsOfType<Enemy>().Length; 
+        if(enemyCount == 0)
+        {
+            SpawnEnemyWave(1);
+        }
+    }
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
+    }
+
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPositionX = Random.Range(-spawnRange, spawnRange);
+        float spawnPositionZ = Random.Range(-spawnRange, spawnRange);
+
+        Vector3 randomPosition = new Vector3(spawnPositionX, 0, spawnPositionZ);
+        return randomPosition;
+    }
+}
